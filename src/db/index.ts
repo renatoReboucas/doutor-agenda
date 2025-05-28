29027f07cc68b3dc7e824a7db12261b5c7dd6a6a
renatoReboucas <renato190698@gmail.com>
2025-05-28 16:52:35 -0300

    feat: integrate @neondatabase/serverless and update database connection method

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
 
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+});
