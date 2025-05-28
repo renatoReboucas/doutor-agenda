2fd3f5db1416274cb5e2f4d7b97e66de1e6678e6
renatoReboucas <renato190698@gmail.com>
2025-05-28 09:50:10 -0300

    feat: enhance authentication flow and add sign-out functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,23 @@
-const DashboardPage = () => {
-  return <div>Dashboard</div>;
-};
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/authentication");
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
+    </div>
+  );
+};
 export default DashboardPage;
