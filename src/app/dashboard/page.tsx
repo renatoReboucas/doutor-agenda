1047cb35e93a51496c8a9e34e43d41fef9d74a9b
renatoReboucas <renato190698@gmail.com>
2025-05-28 10:37:00 -0300

    feat: integrate @radix-ui/react-dialog and enhance database schema

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -8,9 +11,13 @@ const DashboardPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  if (!session) {
-    redirect("/authentication");
-  }
+  if (!session) redirect("/authentication");
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) redirect("/clinic-form");
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1>Dashboard</h1>
